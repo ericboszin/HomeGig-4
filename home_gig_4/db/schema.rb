d3ca94ca896660817ca5e162b26965d8c8360813
Paul <poying.chen@mail.utoronto.ca>
2019-10-29 10:55:20 -0400

    Added first and last name, birthday to User. Can now edit profile, delete account.

--- home_gig_4/db/schema.rb
+++ home_gig_4/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_134609) do
+ActiveRecord::Schema.define(version: 2019_10_29_142149) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 2019_10_29_134609) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
