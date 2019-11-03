7b78c183cf9d87b469a300d2cbd05e5b071193d9
Brian Aly <brianaly1@github.com>
2019-11-02 17:56:15 -0700

    Added roles enum to users

--- home_gig_4/app/models/user.rb
+++ home_gig_4/app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   enum role: [:admin, :owner, :worker]
   has_many :jobs, dependent: :destroy
-
+  has_many :bids, through: :jobs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bids, dependent: :destroy
 end
