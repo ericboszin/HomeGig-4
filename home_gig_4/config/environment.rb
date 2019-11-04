ae67af745b07214b0bb8aedc23cf650364280d8b
Paul <poying.chen@mail.utoronto.ca>
2019-11-04 11:01:45 -0500

    Email confirmation is complete, with credentials encrypted.

--- home_gig_4/config/environment.rb
+++ home_gig_4/config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => Rails.application.credentials.sendgrid[:userName],
+  :password => Rails.application.credentials.sendgrid[:password],
+  :domain => Rails.application.credentials.sendgrid[:domain],
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
