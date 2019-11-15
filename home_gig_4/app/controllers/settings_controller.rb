6673338f856b20f649b96b48060d4ad34790d54e
Paul Chen <poying.chen@mail.utoronto.ca>
2019-11-14 19:44:20 -0500

    Created route for settings

--- home_gig_4/app/controllers/settings_controller.rb
+++ home_gig_4/app/controllers/settings_controller.rb
@@ -1,4 +1,5 @@
 class SettingsController < ApplicationController
+  before_action :setting_owner
 	def index
 		@settings = current_user.setting
 	end
@@ -8,4 +9,12 @@ class SettingsController < ApplicationController
 
 	def create
 	end
+
+	def setting_owner
+			    @setting = Setting.find_by(user_id: params[:id])
+			if @setting.user_id != current_user.id
+      				redirect_to(root_path)
+      		end
+    end
+
 end
