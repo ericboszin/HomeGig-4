65a2381ac41d0c494af441d5382bfaf8421b3cab
Paul Chen <poying.chen@mail.utoronto.ca>
2019-11-21 12:33:01 -0500

    Altered migrations for pg

--- home_gig_4/db/migrate/20191115154631_change_skill_to_int.rb
+++ home_gig_4/db/migrate/20191115154631_change_skill_to_int.rb
@@ -1,5 +1,5 @@
 class ChangeSkillToInt < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :skill, :integer
+    change_column :users, :skill, 'integer USING CAST(skill AS integer)'
   end
 end
--- home_gig_4/db/schema.rb
+++ home_gig_4/db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2019_11_19_163455) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bids", force: :cascade do |t|
     t.string "description"
     t.float "amount"
-    t.integer "job_id", null: false
+    t.bigint "job_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
@@ -47,7 +50,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_163455) do
   create_table "reviews", force: :cascade do |t|
     t.string "text"
     t.integer "rating"
-    t.integer "job_id", null: false
+    t.bigint "job_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
