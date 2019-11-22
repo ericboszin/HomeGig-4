d60bdc927cfd32cd2240c2dfc6129086b204c639
jose-cabrera-ormaza <josemanuel.cabreraormaza@mail.utoronto.ca>
2019-11-22 03:50:28 -0500

    fixing popper module not found heroku issue

--- home_gig_4/app/javascript/packs/application.js
+++ home_gig_4/app/javascript/packs/application.js
@@ -8,8 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("popper")
 require("bootstrap")
+require("./src/application.scss")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
