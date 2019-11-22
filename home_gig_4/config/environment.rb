7e1dfee7eeddfb9e848ce2f8bba4dcdc0a0ba377
Paul Chen <poying.chen@mail.utoronto.ca>
2019-11-22 16:54:16 -0500

    Disabled emails for now

--- home_gig_4/config/environment.rb
+++ home_gig_4/config/environment.rb
@@ -5,24 +5,24 @@ require_relative 'application'
 Rails.application.initialize!
 
 
-ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'home-gig-4.herokuapp.com',
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
-
-
-
 #ActionMailer::Base.smtp_settings = {
- #:user_name => Rails.application.credentials.mailgun[:userName],
- #:password => Rails.application.credentials.mailgun[:password],
- #:domain => Rails.application.credentials.mailgun[:domain],
- #:address => 'smtp.mailgun.org',
- #:port => 587,
- #:authentication => :plain,
- #:enable_starttls_auto => true
+  #:port           => ENV['MAILGUN_SMTP_PORT'],
+ # :address        => ENV['MAILGUN_SMTP_SERVER'],
+#  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+#  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+#  :domain         => 'home-gig-4.herokuapp.com',
+#  :authentication => :plain,
 #}
+#ActionMailer::Base.delivery_method = :smtp
+
+
+
+ActionMailer::Base.smtp_settings = {
+ :user_name => Rails.application.credentials.mailgun[:userName],
+ :password => Rails.application.credentials.mailgun[:password],
+ :domain => Rails.application.credentials.mailgun[:domain],
+ :address => 'smtp.mailgun.org',
+ :port => 587,
+ :authentication => :plain,
+ :enable_starttls_auto => true
+}
