dd2d3fa31fc619bbadb6d9d21a9f31c96192ba31
Paul Chen <poying.chen@mail.utoronto.ca>
2019-11-22 15:03:22 -0500

    changed mailgun credentials for heroku

--- home_gig_4/config/environment.rb
+++ home_gig_4/config/environment.rb
@@ -10,7 +10,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'home-gig-4,herokuapp.com',
+  :domain         => 'home-gig-4.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
