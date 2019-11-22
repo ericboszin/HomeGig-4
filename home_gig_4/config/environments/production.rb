2eccf53afc553f0427ec9d8c20ef37f099f3f6a2
Paul Chen <poying.chen@mail.utoronto.ca>
2019-11-22 15:28:26 -0500

    Edited mailgun settings

--- home_gig_4/config/environments/production.rb
+++ home_gig_4/config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.smtp_settings = { :host => 'home-gig-4.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'home-gig-4.herokuapp.com'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
