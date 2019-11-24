d3a3f4df9aac40a32a4c8702ee07f00a202cfdce
iabouhashish <ibrahim.abouhashish@mail.utoronto.ca>
2019-11-24 16:47:48 -0500

    Removed jobs index route

--- home_gig_4/app/controllers/jobs_controller.rb
+++ home_gig_4/app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
 
     def index
-        @jobs = Job.all
+        redirect_to root_path
     end
 
     def show
