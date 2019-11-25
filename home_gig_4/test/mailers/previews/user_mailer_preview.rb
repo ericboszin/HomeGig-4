8afd486189aaa97bc39810f0fc6dd9d34c325e37
Paul Chen <poying.chen@mail.utoronto.ca>
2019-11-24 19:30:55 -0500

    Comfirmation instructions done. Started using email previews

--- home_gig_4/test/mailers/previews/user_mailer_preview.rb
+++ home_gig_4/test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+	def bid_created_email
+    	UserMailer.with(user: User.first, job: Job.first, creator: User.last).bid_created_email
+        
+  	end
 end
