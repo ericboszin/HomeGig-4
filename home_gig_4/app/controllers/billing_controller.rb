a4e4702297526b3a9551d79a8ae2378ea71e6fbc
jose-cabrera-ormaza <josemanuel.cabreraormaza@mail.utoronto.ca>
2019-11-26 05:12:25 -0500

    fetching job and bid models in create action billing controller

--- home_gig_4/app/controllers/billing_controller.rb
+++ home_gig_4/app/controllers/billing_controller.rb
@@ -11,6 +11,12 @@ class BillingController < ApplicationController
 
     def create
         Stripe.api_key =  "sk_test_cwl4g4GFYZaZ9dtfQ7BLWQyb00x6deRBFY"
+        token  = params[:stripeToken]
+        job_id = params[:jobId]
+        bid_id = params[:bidId]
+        @job = Job.find(job_id)
+        @bid = @job.bids.find(bid_id)
+
         charge = Stripe::Charge.create({
             amount: (@bid.amount.to_i),
             currency: 'usd',
@@ -18,5 +24,5 @@ class BillingController < ApplicationController
             source: token,
         })
     end
-
+    
 end
