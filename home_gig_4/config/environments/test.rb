31d61b0772adc9eee942ff72e4ad7aeb8c14efc5
iabouhashish <ibrahim.abouhashish@mail.utoronto.ca>
2019-11-26 18:16:46 -0500

    Faulty testing

--- home_gig_4/config/environments/test.rb
+++ home_gig_4/config/environments/test.rb
@@ -16,13 +16,13 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{1.hour.to_i}"
+    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
-  config.action_mailer.default_url_options = {:host => 'localhost', port: 3000}
+  # config.action_mailer.default_url_options = { :host => 'localhost', port: 3000}
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
@@ -48,3 +48,6 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 end
+
+# Rails.application.routes.default_url_options[:host] = "localhost"
+# Rails.application.routes.default_url_options[:port] = 3000
