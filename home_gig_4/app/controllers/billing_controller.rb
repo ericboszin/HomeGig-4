130dc05cdb141803e7f41a9162ecd21990c4fb8e
jose-cabrera-ormaza <josemanuel.cabreraormaza@mail.utoronto.ca>
2019-11-26 01:20:50 -0500

    added create action in billing controller

--- home_gig_4/app/controllers/billing_controller.rb
+++ home_gig_4/app/controllers/billing_controller.rb
@@ -1,7 +1,21 @@
 class BillingController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :authenticate_user!
     def new
+        @bid = params[:bid]
     end
 
-    def charge
+    def create
+        Stripe.api_key =  "sk_test_cwl4g4GFYZaZ9dtfQ7BLWQyb00x6deRBFY"
+        token = params[:stripeToken]
+
+        charge = Stripe::Charge.create({
+            amount: 999,
+            currency: 'usd',
+            description: 'Example charge',
+            source: token,
+        })
+
+        redirect_to root_path
     end
 end
