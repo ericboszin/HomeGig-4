d0fb5c42f1ad150eb4c276b35d242c4e0b0ec4b2
jose-cabrera-ormaza <josemanuel.cabreraormaza@mail.utoronto.ca>
2019-11-26 05:10:46 -0500

    adding jobId and bidId to new action in billing controller

--- home_gig_4/app/controllers/billing_controller.rb
+++ home_gig_4/app/controllers/billing_controller.rb
@@ -1,21 +1,22 @@
 class BillingController < ApplicationController
     skip_before_action :verify_authenticity_token
     before_action :authenticate_user!
+
     def new
-        @bid = params[:bid]
+        job_id = params[:jobId]
+        bid_id = params[:bidId]
+        @job = Job.find(job_id)
+        @bid = @job.bids.find(bid_id)
     end
 
     def create
         Stripe.api_key =  "sk_test_cwl4g4GFYZaZ9dtfQ7BLWQyb00x6deRBFY"
-        token = params[:stripeToken]
-
         charge = Stripe::Charge.create({
-            amount: 999,
+            amount: (@bid.amount.to_i),
             currency: 'usd',
             description: 'Example charge',
             source: token,
         })
-
-        redirect_to root_path
     end
+
 end
