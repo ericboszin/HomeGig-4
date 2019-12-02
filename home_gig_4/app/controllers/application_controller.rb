766d521e0baae124a841c94712e196a395353a01
iabouhashish <ibrahim.abouhashish@mail.utoronto.ca>
2019-12-02 17:52:23 -0500

    404 page

--- home_gig_4/app/controllers/application_controller.rb
+++ home_gig_4/app/controllers/application_controller.rb
@@ -23,4 +23,8 @@ class ApplicationController < ActionController::Base
       return root_path
     end
   end
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
 end
