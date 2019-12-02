301c74c4b62c8f0a293a2086c8efd22c039a6195
jose-cabrera-ormaza <josemanuel.cabreraormaza@mail.utoronto.ca>
2019-12-01 22:39:22 -0500

    updating billing controller to charge after bids confirmation

--- home_gig_4/app/controllers/billing_controller.rb
+++ home_gig_4/app/controllers/billing_controller.rb
@@ -10,34 +10,49 @@ class BillingController < ApplicationController
     end
 
     def create
-        Stripe.api_key =  "sk_test_cwl4g4GFYZaZ9dtfQ7BLWQyb00x6deRBFY"
-        token  = params[:stripeToken]
-        job_id = params[:jobId]
-        bid_id = params[:bidId]
-        @job = Job.find(job_id)
-        @bid = @job.bids.find(bid_id)
-
-        charge = Stripe::Charge.create({
-            amount: (@bid.amount.to_i)*100,
-            currency: 'usd',
-            description: 'Example charge',
-            source: token,
-        })
-
+      if current_user.role == "owner"
+        @job = Job.find(params[:jobId])
         @user = User.find(@job.user_id)
+        Stripe.api_key =  "sk_test_cwl4g4GFYZaZ9dtfQ7BLWQyb00x6deRBFY"
+        token = params[:stripeToken]
         if @job.status == "cancelled" || @job.status == "completed"
-          flash[:warning] = "Job has already been marked completed or it has been cancelled"
+          flash[:warning] = "Error: Cannot accept bids on a completed or cancelled job"
+          redirect_to job_path(@job)
+        else
+          if current_user == @user
+            selected_bids = false
+            @job.bids.each do |_bid|
+              if (_bid.selected == 1) #Bid was selected
+                selected_bids = true
+
+                charge = Stripe::Charge.create({
+                  amount: (_bid.amount.to_i)*100,
+                  currency: 'usd',
+                  description: 'Example charge',
+                  source: token,
+                })
+
+                @bidder = User.find(_bid.user_id)
+                if @bidder.setting.job_started
+                  UserMailer.with(user: User.find(@job.user_id), job: @job, bidder: @bidder).job_started_email.deliver_now
+                end
+              end
+            end
+            if selected_bids == false
+              flash[:warning] = "Error: You have not accepted a bid"
+              redirect_to job_path(@job)
+            else
+              @job.status = "started"
+            end
+            @job.save
+          else
+            flash[:warning] = "Error: user not authorized to accept bid"
+          end
           redirect_to job_path(@job)
         end
-        if current_user == @user
-          @bid.selected = 1
-          @bid.save
-          if User.find(@bid.user_id).setting.bid_created
-            UserMailer.with(user: User.find(@bid.user_id), job: @job, owner: User.find(current_user.id)).bid_accepted_email.deliver_now
-          end
-        else
-          flash[:warning] = "Error: user not authorized to accept bid"
-        end
-        redirect_to job_bids_path(job_id)
+      else
+        flash[:warning] = "Error: workers cannot accept bids"
+        redirect_to job_path(@job)
+      end
     end
 end
